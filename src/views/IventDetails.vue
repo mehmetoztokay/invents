4c00255a56e9f80930ec59381c19eefab65525b9
mehmetoztokay <mehmetoztokay@gmail.com>
2022-06-22 19:36:12 +0300

    Added ivent details area of Ivent page.

--- src/views/IventDetails.vue
+++ src/views/IventDetails.vue
@@ -40,6 +40,36 @@
       </div>
     </div>
 
+    <div class="ivent-details">
+      <div class="container">
+        <div class="text-area">
+          <p class="title">İçerik</p>
+          <p class="text">
+            Kısaca özetlemek gerekirse bu oyun bir "yanlışlıklar komedisi"dir. İlliria Krallığı'nda geçer.Shakespeare'in bu krallığı İtalya'nın Dalmaçya kıyısında hayal ettiği
+            düşünülür. İnsanların kılık değiştirdiği, kadınların erkek, erkeklerin kadın kılığına girdiği, birbirlerine aşık olan insanların bu karmaşayı anlamaya çalıştığı ve
+            elbette sonunda her şeyin tatlıya bağlandığı bir eserdir. Tarz olarak Bir Yaz Gecesi Rüyası'na benzer. Soytarılar, sarhoşlar, çıkarcılar, mevki düşkünleri, müzisyenler,
+            deliler ve soyluların iç içe; burun buruna olduğu tabiri caizse oyun içinde oyundur.
+            <br />
+            <br />
+
+            Kısaca özetlemek gerekirse bu oyun bir "yanlışlıklar komedisi"dir. İlliria Krallığı'nda geçer.Shakespeare'in bu krallığı İtalya'nın Dalmaçya kıyısında hayal ettiği
+            düşünülür. İnsanların kılık değiştirdiği, kadınların erkek, erkeklerin kadın kılığına girdiği, birbirlerine aşık olan insanların bu karmaşayı anlamaya çalıştığı ve
+            elbette sonunda her şeyin tatlıya bağlandığı bir eserdir. Tarz olarak Bir Yaz Gecesi Rüyası'na benzer. Soytarılar, sarhoşlar, çıkarcılar, mevki düşkünleri, müzisyenler,
+            deliler ve soyluların iç içe; burun buruna olduğu tabiri caizse oyun içinde oyundur.
+          </p>
+        </div>
+
+        <div class="pic-area">
+          <div class="pic-item">
+            <img src="/src/assets/img/ivents/ivents-list/on-ikinci-gece/main.jpg" alt="" />
+          </div>
+          <div class="pic-item">
+            <img src="/src/assets/img/ivents/ivents-list/on-ikinci-gece/main.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <appFooter />
   </div>
 </template>
@@ -183,6 +213,162 @@
               .icon {
                 margin-right: rem(7);
               }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .ivent-details {
+    padding: rem(100) 0;
+
+    @media screen and (max-width: $screen-md) {
+      padding: rem(50) 0;
+    }
+
+    .container {
+      display: flex;
+      flex-wrap: wrap;
+      .text-area {
+        width: 50%;
+        padding-right: rem(30);
+        .title {
+          font-size: rem(40);
+          font-weight: $weight-700;
+          color: $color-black;
+        }
+        .text {
+          margin-top: rem(20);
+          color: $color-black-off;
+        }
+      }
+
+      .pic-area {
+        width: 50%;
+        position: relative;
+        padding-top: rem(50);
+
+        .pic-item {
+          overflow: hidden;
+
+          height: rem(500);
+          width: 75%;
+          margin-bottom: 20%;
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: rem(10);
+          }
+
+          &:nth-child(2) {
+            width: 70%;
+            position: absolute;
+            top: 20%;
+            left: 20%;
+          }
+        }
+      }
+
+      @media screen and (max-width: $screen-lg) {
+        .text-area {
+          width: 50%;
+          padding-right: 4%;
+          .title {
+            font-size: rem(30);
+          }
+          .text {
+            margin-top: rem(15);
+          }
+        }
+
+        .pic-area {
+          width: 50%;
+          position: relative;
+
+          .pic-item {
+            overflow: hidden;
+            height: rem(350);
+
+            &:nth-child(2) {
+              height: rem(300);
+            }
+          }
+        }
+      }
+
+      @media screen and (max-width: $screen-md) {
+        .text-area {
+          width: 100%;
+          padding-right: 0;
+          .title {
+            font-size: rem(35);
+          }
+          .text {
+            margin-top: rem(10);
+          }
+        }
+
+        .pic-area {
+          width: 100%;
+          position: relative;
+          display: flex;
+          flex-wrap: wrap;
+          margin-top: rem(30);
+
+          .pic-item {
+            overflow: hidden;
+            border-radius: rem(10);
+            height: rem(350);
+            width: 50%;
+            margin-bottom: rem(30);
+            padding: rem(5);
+
+            &:nth-child(2) {
+              width: 50%;
+              position: static;
+              height: rem(350);
+            }
+          }
+        }
+      }
+
+      @media screen and (max-width: $screen-md) {
+        .text-area {
+          order: 1;
+          width: 100%;
+          padding-right: 0;
+          margin-top: rem(30);
+          .title {
+            font-size: rem(35);
+          }
+          .text {
+            margin-top: rem(10);
+          }
+        }
+
+        .pic-area {
+          order: 0;
+          width: 100%;
+          position: relative;
+          display: flex;
+          flex-wrap: wrap;
+          margin-top: rem(0);
+          padding-top: 0;
+
+          .pic-item {
+            overflow: hidden;
+            border-radius: rem(10);
+            height: rem(350);
+            width: 100%;
+            margin-bottom: rem(15);
+            padding: rem(2);
+
+            &:nth-child(2) {
+              width: 100%;
+              position: static;
+              height: rem(350);
             }
           }
         }
